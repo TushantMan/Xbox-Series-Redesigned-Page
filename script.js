10b36940176b1aa92d00347d7353aaa69efe4762
Tushant Man <tushantman77@hotmail.com>
2021-06-21 22:38:07 +0545

    commit

--- script.js
+++ script.js
@@ -26,3 +26,13 @@ for (var i = 0; i < navLinks.length; i++) {
     menuBtn.style.pointerEvents = "auto";
   });
 }
+const scrollBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll", function () {
+  scrollBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
